0a175547d8d7291f4daec88c278e64b1a0f75b70
MD ABU SUFIAN <sufianabu206@gmail.com>
2026-05-19 15:22:15 +0600

    Implement Vines to trap spawn

---
+++ Assets/Scripts/CagneyCarnation/VinesToFlyTrap.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace CagneyCarnation
+{
+    public class VinesToFlyTrap : MonoBehaviour
+    {
+       
+        [SerializeField] private GameObject flyTrapPrefab;
+        [SerializeField] Transform flyTrapSpawnPoint;
+
+        [SerializeField] private float vinesActiveTime = 3f;
+
+        void OnSpawnedFlyTrapEvent()
+        {
+            SpawnedFlyTrap();
+        }
+
+        void SpawnedFlyTrap()
+        {
+            Instantiate(flyTrapPrefab, flyTrapSpawnPoint.position, Quaternion.identity);
+             Invoke(nameof(DeactivateVines), vinesActiveTime);
+        }
+
+        void DeactivateVines()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
